261d0f318a264f02fbc71f615c213ed01a998d0b
DannyEspinosa <dannythebeatle16@gmail.com>
2023-06-16 14:22:31 -0600

    se agrego banderas

---
+++ Unidad1/11.Banderas/src/pkg11/banderas/Banderas.java
@@ -0,0 +1,27 @@
+package pkg11.banderas;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Banderas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edadJuan = 22;
+        int edadPedro = 20;
+        boolean bandera1;
+        
+        
+        bandera1 = (edadJuan > edadPedro);
+        System.out.println("Resultado de bandera: "+bandera1);
+        int edadLupe = 35;
+        boolean bandera2;
+        boolean bandera3;
+        bandera2 = (edadLupe < edadPedro); 
+    }
+    
+}
+
