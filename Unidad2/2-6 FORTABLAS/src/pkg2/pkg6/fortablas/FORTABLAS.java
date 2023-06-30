322562f8be90ed9c5cde253c3e146578f5c6113b
DannyEspinosa <dannythebeatle16@gmail.com>
2023-06-30 12:03:54 -0600

    Se agrego for tablas

---
+++ Unidad2/2-6 FORTABLAS/src/pkg2/pkg6/fortablas/FORTABLAS.java
@@ -0,0 +1,24 @@
+
+package pkg2.pkg6.fortablas;
+
+/**
+ *
+ * @author Daniel
+ */
+public class FORTABLAS {
+
+  
+    public static void main(String[] args) {
+         int i,j;
+     for(i=1; i<=10; i++){
+         System.out.println("TABLA DEL " +i );
+         System.out.println("-------------------");
+         
+         for( j=1; j<=10; j++){
+             System.out.println(j + "x" + j + "=" + j*i);
+         }
+     }
+
+    }
+    
+}
