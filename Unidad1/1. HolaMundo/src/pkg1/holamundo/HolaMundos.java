b5c8cb7c4e10595f1eb17d5b37119699a0511595
DannyEspinosa <dannythebeatle16@gmail.com>
2023-09-05 20:36:14 -0600

    Subimos holamundos

---
+++ Unidad1/1. HolaMundo/src/pkg1/holamundo/HolaMundos.java
@@ -0,0 +1,17 @@
+
+package pkg1.holamundo;
+
+/**
+ *
+ * @author Daniel
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
